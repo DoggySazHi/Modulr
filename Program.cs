d6bef6bb89241d05d1513c1b67eea797705a2a57
William Le <william.le.huy@gmail.com>
2020-09-06 23:37:22 -0700

    Setting up Docker configurations for testing...

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Modulr
@@ -10,8 +11,12 @@ namespace Modulr
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("config.json", false, false);
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>().UseUrls("http://*:5001"); });
     }
 }
---
+++ Tester/JavaUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Modulr.Tester
+{
+    internal class TesterConfiguration
+    {
+        private string JDKPath { get; set; }
+        private bool UseDocker { get; set; }
+    }
+    
+    public static class JavaUtils
+    {
+        private static readonly TesterConfiguration Config;
+        
+        static JavaUtils()
+        {
+            Config = JsonSerializer.Deserialize<TesterConfiguration>("config.json");
+        }
+    }
+}
